189541a5585cc6915a9b568d0ee0ada7464c25f8
Ian63271 <144076239+Ian63271@users.noreply.github.com>
2023-11-10 21:08:39 -0600

    Update ControlUnit.v
    added MemToReg output

--- ControlUnit.v
+++ ControlUnit.v
@@ -2,6 +2,7 @@ module ControlUnit (
     input [5:0]opcode,
     output reg MemToReg,
     output reg MemToWrite,
+    output reg MemToRead,
     output reg [1:0]ALUOp,
     output reg RegWrite
 );
@@ -11,12 +12,14 @@ always @(*) begin //cada caso sera un tipo de instruccion diferente.
         6'b000000: begin //se usa el begin y end porque por cualquier razon no deja asignar señales de un bit sin esto.
             MemToReg = 1'b0;
             MemToWrite = 1'b0;
+            MemToRead = 1'b0;
             ALUOp = 2'b10; 
             RegWrite = 1'b1;
         end
         default: begin
             MemToReg = 1'b0;
             MemToWrite = 1'b0;
+            MemToRead = 1'b0;
             ALUOp = 2'b00; 
             RegWrite = 1'b0;
         end
