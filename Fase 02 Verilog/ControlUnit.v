7dd9fe758fd0c1ed08840db383050a1d3be183e0
Ian63271 <144076239+Ian63271@users.noreply.github.com>
2023-11-21 21:34:49 -0600

    Added fixes to verilog code

---
+++ Fase 02 Verilog/ControlUnit.v
@@ -0,0 +1,110 @@
+module ControlUnit (
+    input [5:0]opcode, // lo que recibe de la instruccion
+    output reg regDst, // indica de donde se escribe en el registro
+    output reg branch, // indica si se hara un salto
+    output reg MemToRead, //no se usa en esta fase, si se lee de memoria
+    output reg MemToReg, // este ahora va al mux, si se sacara de la memoria al registro
+    output reg [1:0]ALUOp, //codigo para la alu control, esto sirve para type r, sw y lw.
+    output reg MemToWrite, //no se usa en esta fase
+    output reg ALUSrc, //de donde recibe la alu su segundo operando
+    output reg RegWrite //si se escribe en el registro
+    
+    
+);
+
+always @(*) begin //cada caso sera un tipo de instruccion diferente.
+    case (opcode)
+        6'b000000: begin //se usa el begin y end porque por cualquier razon no deja asignar señales de un bit sin esto.
+            MemToReg = 1'b0; 
+            regDst = 1'b1;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0;      /// que hacia branch? // se usa para condicionales
+            ALUOp = 2'b10; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b1;
+        end
+        6'b001000: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1;
+            regDst = 1'b0;
+        end
+        6'b001100: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1;
+            regDst = 1'b0;
+        end
+        6'b001101: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1;
+            regDst = 1'b0;
+        end
+        6'b001010: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1;
+            regDst = 1'b0;
+        end
+        6'b100011: begin
+            MemToReg = 1'b1;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b1;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1;
+            regDst = 1'b0;
+        end
+        6'b101011: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b1;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b0;
+            regDst = 1'b0;
+        end
+        6'b000100: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b1; 
+            ALUOp = 2'b01; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b0;
+            regDst = 1'b0;
+        end
+        default: begin
+            MemToReg = 1'b0;
+            MemToWrite = 1'b0;
+            MemToRead = 1'b0;
+            branch = 1'b0; 
+            ALUOp = 2'b00; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b0;
+            regDst = 1'b0;
+        end
+    endcase
+end
+
+endmodule
---
+++ Fase 02 Verilog/PFF2.v
@@ -0,0 +1,166 @@
+module ProyectoFinalFase2 (input clk);
+
+//Instrucciones
+wire [31:0]instruccion;
+
+wire [5:0] OP;
+wire [4:0] RS;
+wire [4:0] RT;
+wire [4:0] RD;
+wire [4:0] Shamt;
+wire [5:0] Funct;
+wire [15:0] Inst;
+wire [31:0] SEInst;
+
+//PC
+reg [31:0]dir;
+wire [31:0]PcOUT;
+reg [31:0]sum4pcout;
+
+//Registros y ALU
+wire [31:0]C1;
+wire [31:0]C2;
+wire [31:0]C3;
+wire [3:0]C4;
+wire [31:0]C5;
+reg [31:0]MUX;
+
+//Control Unit
+wire regdst; // de control unit al mux1
+wire branch;
+wire regwrite;
+wire memwrite;
+wire memread;
+wire memreg;
+wire [1:0]ALUop;
+wire alusrc;
+
+// mux reg
+reg [4:0]writereg;
+
+//mux alu
+reg [31:0]op2;
+
+//mux pc
+reg ANDpcmux;
+reg [31:0]brALU;
+reg [31:0]BitShiftInst;
+
+PC pCounter(
+    .pcin(dir),
+    .pcout(PcOUT)
+);
+
+
+
+MemINS MemInst(
+    .readAddress(PcOUT),
+    .Instruccion(instruccion) 
+);
+
+
+ControlUnit CU (
+    .opcode(OP),
+    .regDst(regdst),
+    .branch(branch),
+    .MemToRead(memread),
+    .MemToReg(memreg),
+    .ALUOp(ALUop),
+    .MemToWrite(memwrite),
+    .ALUSrc(alusrc),
+    .RegWrite(regwrite)
+);
+
+MemREG Register (
+    .Dir1(RS),
+    .Dir2(RT),
+    .DirWrite(writereg),
+    .DatoNuevo(MUX), //Es el dato que se escribe, resultado de la ALU.
+    .Dato1(C2), //for some reason el cable2 se conecta al dato y viceversa.
+    .Dato2(C1),
+    .RWEN(regwrite)
+);
+
+ALUControl ALUcon (
+    .FuncIn(Funct),
+    .ALUOp(ALUop),
+    .ALUin(C4)
+);
+
+ALU ALUins (
+    .OP1(C2),
+    .OP2(op2),
+    .Sel(C4),
+    .ZF(TRZF),
+    .resultado(C3)
+);
+
+MemRAM Mem(
+    .rEn(memread),
+    .wEn(memwrite),
+    .Adress(C3),
+    .DataWrite(C1),
+    .DataRead(C5)
+);
+
+SignExtend SE(
+    .preExt(Inst),
+    .ext(SEInst)
+);
+
+
+
+// initial begin
+//        $readmemb("output.txt",MemInst.INS);
+// end
+
+assign OP = instruccion [31:26];
+assign RS = instruccion [25:21];
+assign RT = instruccion [20:16];
+assign RD = instruccion [15:11];
+assign Shamt = instruccion [10:6];
+assign Funct = instruccion [5:0];
+assign Inst = instruccion [15:0];
+
+always @(posedge clk) begin
+    
+    sum4pcout <= PcOUT + 4; //se suma 4 a la direccion, esto entrara al PC
+    
+end
+
+always @(*) begin //MUX varios del dptr
+    
+    BitShiftInst <= SEInst << 2; // bitshift a la izq en la instruccion ya extendida a 32 bits.
+    brALU <= sum4pcout + SEInst;
+    ANDpcmux <= branch & TRZF;
+
+    case (memreg)
+        1'b0: 
+            MUX = C3;
+        default: 
+            MUX = C5;
+        endcase
+
+        case (regdst)
+        1'b0: 
+            writereg = RT;
+        default: 
+            writereg = RD;
+        endcase
+
+        case (alusrc)
+        1'b0: 
+            op2 = C1;
+        default: 
+            op2 = SEInst;
+        endcase
+
+        case (ANDpcmux)
+        1'b0: 
+            dir = sum4pcout;
+        default: 
+            dir = brALU;
+        endcase
+end
+
+endmodule
